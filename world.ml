149b372d645b77ae6ce386d34ca5cb48a1aa8325
Geoff Hulette <geoff@hulette.net>
2022-04-19 12:28:53 -0700

    Format

--- world.ml
+++ world.ml
@@ -1,43 +1,27 @@
 let enumf f =
-  let rec aux acc = function
-    | 0 -> acc
-    | x -> aux (f x :: acc) (x - 1)
-  in
+  let rec aux acc = function 0 -> acc | x -> aux (f x :: acc) (x - 1) in
   aux []
 
 module Pt = struct
-    type t = int * int
+  type t = int * int
 
-    let compare = compare
-    let add (x1,y1) (x2,y2) = (x1 + x2, y1 + y2)
-    let grid n m = List.concat (enumf (fun j -> enumf (fun i -> i,j) n) m)
-  end
+  let compare = compare
+  let add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+  let grid n m = List.concat (enumf (fun j -> enumf (fun i -> (i, j)) n) m)
+end
 
 module Pts = Set.Make (Pt)
 
-type t = {
-    width : int;
-    height : int;
-    alive : Pts.t
-  }
+type t = { width : int; height : int; alive : Pts.t }
 
-let make w h cells =
-  { width = w; height = h; alive = Pts.of_list cells }
-
+let make w h cells = { width = w; height = h; alive = Pts.of_list cells }
 let empty w h = make w h []
 
 let random w h =
   let rpts = List.filter (fun _ -> Random.bool ()) (Pt.grid w h) in
   make w h rpts
 
-let is_alive o x y =
-  Pts.mem (x,y) o.alive
-
-let kill o x y =
-  { o with alive = Pts.remove (x,y) o.alive }
-
-let spawn o x y =
-  { o with alive = Pts.add (x,y) o.alive }
-
-let flip o x y =
-  if is_alive o x y then kill o x y else spawn o x y
+let is_alive o x y = Pts.mem (x, y) o.alive
+let kill o x y = { o with alive = Pts.remove (x, y) o.alive }
+let spawn o x y = { o with alive = Pts.add (x, y) o.alive }
+let flip o x y = if is_alive o x y then kill o x y else spawn o x y
--- world.mli
+++ world.mli
@@ -1,14 +1,15 @@
 type t
 
-(** Create a new empty world *)
 val empty : int -> int -> t
+(** Create a new empty world *)
 
+val random : int -> int -> t
 (** Create a new random world (each cell has a 50% chance of being
 alive *)
-val random : int -> int -> t
 
-(** Testing cells, and turning them on and off *)
 val is_alive : t -> int -> int -> bool
+(** Testing cells, and turning them on and off *)
+
 val kill : t -> int -> int -> t
 val spawn : t -> int -> int -> t
 val flip : t -> int -> int -> t
